54ccf85487386f790431413c85bd22423167f782
Tom Dudfield <tomdudfield@redweb.com>
2015-11-15 16:27:34 +0000

    connect api

--- src/HowWasYourDay/Controllers/HomeController.cs
+++ src/HowWasYourDay/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
 
 namespace HowWasYourDay.Controllers
 {
@@ -10,6 +14,25 @@ namespace HowWasYourDay.Controllers
     {
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string image)
+        {
+            EmotionServiceClient emotionServiceClient = new EmotionServiceClient("e5d15d97c41c4aa7bf042af46bb02e3f");
+
+            byte[] fileBytes = Convert.FromBase64String(image.Replace("data:image/jpeg;base64,", ""));
+
+            using (MemoryStream ms = new MemoryStream(fileBytes))
+            {
+                var s = emotionServiceClient.RecognizeAsync(ms);
+                Emotion[] r = s.Result;
+                
+                //MediaTypeNames.Image streamImage = MediaTypeNames.Image.FromStream(ms);
+                //context.Response.ContentType = "image/jpeg";
+                //streamImage.Save(context.Response.OutputStream, ImageFormat.Jpeg);
+                //return streamImage;
+            }
             return View();
         }
 
